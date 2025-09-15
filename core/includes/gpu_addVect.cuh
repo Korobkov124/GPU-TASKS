0b890f4f65f57c240403fb9c99e38b2c2142186f
Andrey <andreyka.korobkov.97@mail.ru>
2025-09-15 23:44:57 +0700

    Create build files to AddVect lib

--- core/includes/gpu_addVect.cuh
+++ core/includes/gpu_addVect.cuh
@@ -1,10 +1,14 @@
 #pragma once
 
-namespace AddVect{
-    class AddingVectors{
-        static __global__ void GpuAddVect(float* vect1, float* vect2, float* resultVect, int N);
 
+namespace AddVect{
+
+    static __global__ void GpuAddVect(float* vect1, float* vect2, float* resultVect, int N);
+
+    class AddingVectors{
         public:
-            static void CpuAddVect(float* vect1, float* vect2, float* resultVect, int N);
+            static float* CpuAddVect(float* vect1, float* vect2, float* resultVect, int N);
+
+            static float* RunGpu(float* vect1, float* vect2, float* resultVect, int N);
     };
 }
--- core/src/src1.cu
+++ core/src/src1.cu
@@ -3,15 +3,39 @@
 
 namespace AddVect{
 
-__global__ void AddingVectors::GpuAddVect(float* vect1, float* vect2, float* resultVect, int N){
-    int i = threadIdx.x;
-    resultVect[i] = vect1[i] + vect2[i];
-};
-
-void AddingVectors::CpuAddVect(float* vect1, float* vect2, float* resultVect, int N){
-    for(int i = 0; i < N; i++){
+    __global__ void GpuAddVect(float* vect1, float* vect2, float* resultVect, int N){
+        int i = threadIdx.x;
         resultVect[i] = vect1[i] + vect2[i];
-    }
-}
+    };
+
+    float* AddingVectors::CpuAddVect(float* vect1, float* vect2, float* resultVect, int N){
+        for(int i = 0; i < N; i++){
+            resultVect[i] = vect1[i] + vect2[i];
+        }
+        
+        return resultVect;
+    };
+
+    float* AddingVectors::RunGpu(float* vect1, float* vect2, float* resultVect, int N){
+        float *devVect1, *devVect2, *devResult;
+
+        cudaMalloc((void**)&devVect1, sizeof(float) * N);
+        cudaMalloc((void**)&devVect2, sizeof(float) * N);
+        cudaMalloc((void**)&devResult, sizeof(float) * N);
+
+        cudaMemcpy(devVect1, vect1, sizeof(float) * N, cudaMemcpyHostToDevice);
+        cudaMemcpy(devVect2, vect2, sizeof(float) * N, cudaMemcpyHostToDevice);
+        cudaMemcpy(devResult, resultVect, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+        GpuAddVect <<< 1, N >>>(devVect2, devVect2, resultVect, N);
+
+        cudaMemcpy(resultVect, devResult, sizeof(float), cudaMemcpyDeviceToHost);
+
+        
+        cudaFree(devVect2);
+        cudaFree(devVect2);
+        cudaFree(devResult);
+        return resultVect;
+    };
 
 };
