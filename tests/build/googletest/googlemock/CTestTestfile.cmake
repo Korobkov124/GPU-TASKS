1b01daf1a69582edb7928821dd8250f0dda9667b
BERiG75 <berdinigor25@gmail.com>
2025-09-16 11:37:47 +0000

    added tests build

---
+++ tests/build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /content/GPU-TASKS/tests/googletest/googlemock
+# Build directory: /content/GPU-TASKS/tests/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
