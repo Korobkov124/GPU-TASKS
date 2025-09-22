45279eaf3ce562d915d896ac54334abef9b2fcfc
BERiG75 <berdinigor25@gmail.com>
2025-09-22 16:04:48 +0000

    bench plots added

--- bench/main.cu
+++ bench/main.cu
@@ -6,41 +6,35 @@
 #include "gpu_addVect.cuh"
 
 
-// static void Bench_CPU(benchmark::State& state) {
-//   int N = state.range(0);
-//   std::vector<float> a(N), b(N), cpu_res(N);
+static void Bench_CPU(benchmark::State& state) {
+  int N = state.range(0);
+  std::vector<float> a(N), b(N), cpu_res(N);
 
+  for (int i = 0; i < N; ++i) {
+      a[i] = 0.1f * i;
+      b[i] = 0.2f * i;
+  }
 
-//   std::mt19937 rng(12345);
-//   std::uniform_real_distribution<float> dist(-100.0f, 100.0f);
-//   for (int i = 0; i < N; ++i) {
-//       a[i] = dist(rng);
-//       b[i] = dist(rng);
-//   }
+  for (auto _ : state) {
+        AddVect::AddingVectors::CpuAddVect(a.data(), b.data(), cpu_res.data(), N);
+        benchmark::DoNotOptimize(cpu_res.data());
+    }
+}
 
-//   for (auto _ : state) {
-//         AddVect::AddingVectors::CpuAddVect(a.data(), b.data(), cpu_res.data(), N);
-//         benchmark::DoNotOptimize(cpu_res.data());
-//     }
-// }
+static void Bench_GPU(benchmark::State& state) {
+  int N = state.range(0);
+  std::vector<float> a(N), b(N), gpu_res(N);
 
-// static void Bench_GPU(benchmark::State& state) {
-//   int N = state.range(0);
-//   std::vector<float> a(N), b(N), gpu_res(N);
+  for (int i = 0; i < N; ++i) {
+      a[i] = 0.1f * i;
+      b[i] = 0.2f * i;
+  }
 
-
-//   std::mt19937 rng(12345);
-//   std::uniform_real_distribution<float> dist(-100.0f, 100.0f);
-//   for (int i = 0; i < N; ++i) {
-//       a[i] = dist(rng);
-//       b[i] = dist(rng);
-//   }
-
-//   for (auto _ : state) {
-//         AddVect::AddingVectors::RunGpu(a.data(), b.data(), gpu_res.data(), N);
-//         benchmark::DoNotOptimize(gpu_res.data());
-//     }
-// }
+  for (auto _ : state) {
+        AddVect::AddingVectors::RunGpu(a.data(), b.data(), gpu_res.data(), N);
+        benchmark::DoNotOptimize(gpu_res.data());
+    }
+}
 
 // static void Bench_GPU_Esh(benchmark::State& state) {
 //   int N = state.range(0);
@@ -78,13 +72,13 @@
 //   }
 // }
 
-// BENCHMARK(Bench_CPU)
-//   ->RangeMultiplier(2)
-//   ->Range(1<<10, 1<<22);
+BENCHMARK(Bench_CPU)
+  ->RangeMultiplier(2)
+  ->Range(1<<10, 1<<20);
 
-// BENCHMARK(Bench_GPU)
-//   ->RangeMultiplier(2)
-//   ->Range(1<<10, 1<<22);
+BENCHMARK(Bench_GPU)
+  ->RangeMultiplier(2)
+  ->Range(1<<10, 1<<20);
 
 // BENCHMARK(Bench_GPU_Esh)
 //   ->RangeMultiplier(2)
