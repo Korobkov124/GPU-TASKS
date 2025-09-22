83c192adc7265b6dd2ac7c32f5709ddb093a1add
Korobkov124 <andreyka.korobkov.97@mail.ru>
2025-09-22 23:40:22 +0700

    Updated GPU function

--- core/src/src1.cu
+++ core/src/src1.cu
@@ -10,16 +10,7 @@ namespace AddVect{
     };
 
     void FullGpuAddVect(float* vect1, float* vect2, float* resultVect, int gridSize, float* kernel_ms){
-        float *devVect1, *devVect2, *devResult;
         cudaEvent_t start, stop;
-
-        cudaMalloc((void**)&devVect1, sizeof(float) * gridSize);
-        cudaMalloc((void**)&devVect2, sizeof(float) * gridSize);
-        cudaMalloc((void**)&devResult, sizeof(float) * gridSize);
-
-        cudaMemcpy(devVect1, vect1, sizeof(float) * gridSize, cudaMemcpyHostToDevice);
-        cudaMemcpy(devVect2, vect2, sizeof(float) * gridSize, cudaMemcpyHostToDevice);
-        cudaMemcpy(devResult, resultVect, sizeof(float) * gridSize, cudaMemcpyHostToDevice);
         
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
@@ -30,11 +21,6 @@ namespace AddVect{
         cudaEventRecord(stop);
         cudaEventSynchronize(stop);
         cudaEventElapsedTime(kernel_ms, start, stop);
-
-        cudaMemcpy(resultVect, devResult, sizeof(float) * gridSize, cudaMemcpyDeviceToHost);
-        cudaFree(devVect1);
-        cudaFree(devVect2);
-        cudaFree(devResult);
     };
 
     float* AddingVectors::CpuAddVect(float* vect1, float* vect2, float* resultVect, int gridSize){
