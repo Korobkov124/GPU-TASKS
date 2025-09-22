a2f9e2a8059f583dd3c6f0dd98dfe828ff97017b
Korobkov124 <andreyka.korobkov.97@mail.ru>
2025-09-23 00:04:53 +0700

    Updated headers

--- core/includes/gpu_addVect.cuh
+++ core/includes/gpu_addVect.cuh
@@ -5,7 +5,7 @@ namespace AddVect{
 
     __global__ void GpuAddVect(float* vect1, float* vect2, float* resultVect);
 
-    void FullGpuAddVect(float* vect1, float* vect2, float* resultVect, int N);
+    void FullGpuAddVect(float* vect1, float* vect2, float* resultVect, int gridSize, float* kernel_ms);
 
     class AddingVectors{
         public:
