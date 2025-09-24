76eae3fe5d3087d3a138c02e361e0d0f168d35d1
Korobkov124 <andreyka.korobkov.97@mail.ru>
2025-09-24 23:30:35 +0700

    Updated iterator calculation

--- core/src/src1.cu
+++ core/src/src1.cu
@@ -10,7 +10,7 @@ namespace AddVect{
     }
 
     __global__ void GpuAddVect(float* vect1, float* vect2, float* resultVect){
-        int i = threadIdx.x;
+        int i = blockDim.x * blockIdx.x * threadIdx.x;
         resultVect[i] = vect1[i] + vect2[i];
     };
 
